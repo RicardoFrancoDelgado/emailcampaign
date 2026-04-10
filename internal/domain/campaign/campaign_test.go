6c0e265e8198af8400110bbcb086a9a3d3d9f058
RicardoFrancoDelgado <rf041555@gmail.com>
2026-04-10 20:58:54 -0300

    :recycle: refact: create consts on tests and rename test

--- internal/domain/campaign/campaign_test.go
+++ internal/domain/campaign/campaign_test.go
@@ -7,11 +7,14 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+var (
+	name    = "Campaign X"
+	content = "Body"
+	contact = []string{"email1@e.com", "email2@e.com"}
+)
+
 func Test_NewCampaign_CreateCampaign(t *testing.T) {
 	assert := assert.New(t)
-	name := "Campaign X"
-	content := "Body"
-	contact := []string{"email1@e.com", "email2@e.com"}
 
 	campaign := NewCampaign(name, content, contact)
 
@@ -22,20 +25,14 @@ func Test_NewCampaign_CreateCampaign(t *testing.T) {
 
 func Test_NewCampaign_IDIsNotNill(t *testing.T) {
 	assert := assert.New(t)
-	name := "Campaign X"
-	content := "Body"
-	contact := []string{"email1@e.com", "email2@e.com"}
 
 	campaign := NewCampaign(name, content, contact)
 
 	assert.NotNil(campaign.ID)
 }
 
-func Test_NewCampaign_CreatedOnNotNill(t *testing.T) {
+func Test_NewCampaign_CreatedOnMustBeNow(t *testing.T) {
 	assert := assert.New(t)
-	name := "Campaign X"
-	content := "Body"
-	contact := []string{"email1@e.com", "email2@e.com"}
 	now := time.Now().Add(-time.Minute)
 
 	campaign := NewCampaign(name, content, contact)
