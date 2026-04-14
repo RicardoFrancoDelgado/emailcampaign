076fd28b2be68726d8f73d7f7cfaf9ab0043ae9f
RicardoFrancoDelgado <rf041555@gmail.com>
2026-04-13 23:36:51 -0300

    feat: using validate and test in main

--- internal/domain/campaign/campaign.go
+++ internal/domain/campaign/campaign.go
@@ -14,8 +14,8 @@ type Campaign struct {
 	ID        string    `validate:"required"`
 	Name      string    `validate:"min=5,max=20"`
 	CreatedOn time.Time `validate:"required"`
-	Content   string    `validate:"required"`
-	Contact   []Contact `validate:"min=5"`
+	Content   string    `validate:"min=5,max=1024"`
+	Contact   []Contact `validate:"min=1,dive"`
 }
 
 // método que inicializa uma nova campanha
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"emailcampaing/internal/domain/campaign"
+
+	"github.com/go-playground/validator/v10"
+)
+
+func main() {
+	contacts := []campaign.Contact{{Email: "ricardo@teste.com.br"}, {Email: ""}}
+	campaign := campaign.Campaign{Contact: contacts}
+
+	validate := validator.New()
+	err := validate.Struct(campaign)
+
+	if err == nil {
+		println("Nenhum erro")
+	} else {
+		validationErrors := err.(validator.ValidationErrors)
+		for _, v := range validationErrors {
+
+			switch v.Tag() {
+			case "required":
+				println(v.StructField() + " is required")
+			case "min":
+				println(v.StructField() + " is required with min " + v.Param())
+			case "max":
+				println(v.StructField() + " is required with max " + v.Param())
+			case "email":
+				println(v.StructField() + " is required with max " + v.Param())
+			}
+
+		}
+	}
+}
