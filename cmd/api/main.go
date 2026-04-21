c5af4758abb609388eef59dbd8f572fb8ddc936c
RicardoFrancoDelgado <rf041555@gmail.com>
2026-04-20 23:10:02 -0300

    feat: search for params, routes and query

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,5 +13,21 @@ func main() {
 		w.Write([]byte("Olá lá"))
 	})
 
+	r.Get("/{product}", func(w http.ResponseWriter, r *http.Request) {
+		param := chi.URLParam(r, "product")
+		w.Write([]byte(param))
+	})
+
+	r.Get("/users", func(w http.ResponseWriter, r *http.Request) {
+		name := r.URL.Query().Get("name")
+		age := r.URL.Query().Get("age")
+
+		if name != "" {
+			w.Write([]byte(name + " " + age))
+		} else {
+			w.Write([]byte("users"))
+		}
+	})
+
 	http.ListenAndServe(":3000", r)
 }
